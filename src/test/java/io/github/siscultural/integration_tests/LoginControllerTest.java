46380286c6d2cca20d37e56f62860ff04acc2acc
Victor <victor.hugo.origins@gmail.com>
2016-08-22 11:29:47 -0300

    SimpleGetPagesControllerTest implemented

--- src/test/java/io/github/siscultural/integration_tests/LoginControllerTest.java
+++ src/test/java/io/github/siscultural/integration_tests/LoginControllerTest.java
@@ -25,9 +25,9 @@ import org.springframework.test.context.support.DirtiesContextTestExecutionListe
 import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 /**
@@ -41,7 +41,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @DatabaseSetup(FunctionaryRepositoryIT.DATASET)
 @DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = {FunctionaryRepositoryIT.DATASET})
 @DirtiesContext
-public class LoginControllerTester {
+public class LoginControllerTest {
 
     @Autowired
     private LoginController loginController;
