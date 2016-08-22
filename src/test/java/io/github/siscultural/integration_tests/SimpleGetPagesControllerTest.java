e0616c515b7c01d51465ec9c0989c89810d1dc25
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2016-08-22 13:49:30 -0300

    ignoring some tests

--- src/test/java/io/github/siscultural/integration_tests/SimpleGetPagesControllerTest.java
+++ src/test/java/io/github/siscultural/integration_tests/SimpleGetPagesControllerTest.java
@@ -7,6 +7,7 @@ package io.github.siscultural.integration_tests;
 
 import io.github.siscultural.controllers.SimpleGetPagesController;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
