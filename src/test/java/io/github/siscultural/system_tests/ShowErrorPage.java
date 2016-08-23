0db7ff3456e7a58d9828768ec2fb25c6669e072c
Natarajan Rodrigues <natarajanrodrigues@gmail.com>
2016-08-23 00:50:29 -0300

    bintray config 1

--- src/test/java/io/github/siscultural/system_tests/ShowErrorPage.java
+++ src/test/java/io/github/siscultural/system_tests/ShowErrorPage.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package io.github.siscultural.integration_tests;
+package io.github.siscultural.system_tests;
 
 import org.junit.Assert;
 import org.junit.Test;
