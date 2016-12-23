4795bd61c7c8300b2b8f2451793356d2528f944d
natarajanrodrigues <natarajanrodrigues@gmail.com>
2016-12-22 23:45:56 -0300

    gogo 13

--- src/main/java/io/github/siscultural/Loader.java
+++ src/main/java/io/github/siscultural/Loader.java
@@ -1,11 +1,15 @@
 package io.github.siscultural;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,6 +27,28 @@ public class Loader extends SpringBootServletInitializer {
     public Java8TimeDialect java8TimeDialect() {
         return new Java8TimeDialect();
     }
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        DataSource basicDataSource = new DataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        basicDataSource.setDriverClassName("org.postgresql.Driver");
+        basicDataSource.setTestOnBorrow(true); 
+        basicDataSource.setTestWhileIdle(true); 
+        basicDataSource.setTestOnReturn(true); 
+        basicDataSource.setValidationQuery("SELECT 1");
+
+        return basicDataSource;
+    }
+
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Loader.class, args);
