6eb0590b81c6731b79e0a856d17216af31a471b5
natarajanrodrigues <natarajanrodrigues@gmail.com>
2016-12-23 00:58:44 -0300

    gogo 28

--- src/main/java/io/github/siscultural/DataSourceCustom.java
+++ src/main/java/io/github/siscultural/DataSourceCustom.java
@@ -9,32 +9,40 @@ package io.github.siscultural;
 //import java.net.URI;
 //import java.net.URISyntaxException;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  * Created by natarajan on 23/12/16.
  */
 //@Configuration
 public class DataSourceCustom {
-//
-//    @Bean
-//    @Primary
-//    @Profile("prod")
-//    public BasicDataSource dataSource() throws URISyntaxException {
-//        URI dbUri = new URI(System.getenv("DATABASE_URL"));
-//
-//        String username = dbUri.getUserInfo().split(":")[0];
-//        String password = dbUri.getUserInfo().split(":")[1];
-//        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
-//
-//        BasicDataSource basicDataSource = new BasicDataSource();
-//        basicDataSource.setUrl(dbUrl);
-//        basicDataSource.setUsername(username);
-//        basicDataSource.setPassword(password);
-//        basicDataSource.setDriverClassName("org.postgresql.Driver");
-//        basicDataSource.setTestOnBorrow(true);
-//        basicDataSource.setTestWhileIdle(true);
-//        basicDataSource.setTestOnReturn(true);
-//        basicDataSource.setValidationQuery("SELECT 1");
-//
-//        return basicDataSource;
-//    }
+
+    @Bean
+    @Primary
+    @Profile("prod")
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        DataSource basicDataSource = new DataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+        basicDataSource.setDriverClassName("org.postgresql.Driver");
+        basicDataSource.setTestOnBorrow(true);
+        basicDataSource.setTestWhileIdle(true);
+        basicDataSource.setTestOnReturn(true);
+        basicDataSource.setValidationQuery("SELECT 1");
+
+        return basicDataSource;
+    }
 }
