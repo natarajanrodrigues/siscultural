5315fb37628c2fddf6753e99c8e3da592ab5ed2a
natarajanrodrigues <natarajanrodrigues@gmail.com>
2016-12-23 11:52:03 -0300

    Adding cache to resources

--- src/main/java/io/github/siscultural/MvcConfig.java
+++ src/main/java/io/github/siscultural/MvcConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -33,6 +34,14 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/fornecedores/fornecedor_fisico").setViewName("fornecedores/fornecedor_fisico");
         registry.addViewController("/fornecedores/fornecedor_juridico").setViewName("fornecedores/fornecedor_juridico");
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/")
+                .setCachePeriod(31556926);
     }
 
     @GetMapping("/login")
